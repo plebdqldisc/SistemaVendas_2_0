aff74c04513352626410f07f00c3c1c977207849
ARTHUR ANTONIO DE ARAUJO <arthur.7923@aluno.pr.senac.br>
2023-09-14 17:23:29 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/modelo/Produto.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.modelo;
+
+/**
+ *
+ * @author arthur.7923
+ */
+public class Produto {
+    private Double preco;
+    private String nome;
+
+    public Produto() {
+    }
+
+    public Double getPreço() {
+        return preco;
+    }
+
+    public void setPreço(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/com/mycompany/sistemavendas_2_0/SistemaVendas_2_0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemavendas_2_0;
+
+/**
+ *
+ * @author arthur.7923
+ */
+public class SistemaVendas_2_0 {
+
+    public static void main(String[] args) {
+        
+    }
+}
